b50bfaa28c89adda5702918593f4935d4ea0d38f
SotaNorsu <pelaaja538@gmail.com>
2024-08-10 19:10:40 +0300

    volume set to 0.2

--- res/js/audio_script.js
+++ res/js/audio_script.js
@@ -2,6 +2,7 @@
 
 function music1() {
     var myAudio = new Audio('/res/audio/waitingmusic.mp3'); 
+    myAudio.volume = 0.2;
     myAudio.addEventListener('ended', function() {
         this.currentTime = 0;
         this.play();
