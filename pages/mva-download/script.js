1cef6001583a5e7b7a9ff3b71d225ad40a2c436c
SotaNorsu <pelaaja538@gmail.com>
2024-08-11 08:35:10 +0300

    bg update

--- pages/mva-download/script.js
+++ pages/mva-download/script.js
@@ -1,1 +1,17 @@
-music2();
+
+
+function getURLParameter(name) {
+    return new URLSearchParams(window.location.search).get(name);
+}
+
+
+function checkIfNoAudioFromUrl() {
+    const audio = getURLParameter("noaudio")
+    if (audio == "true") {
+        
+    } else {
+        music2();
+    }
+}
+
+checkIfNoAudioFromUrl();
