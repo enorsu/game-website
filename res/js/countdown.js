c4afd4728daaa0abdc81dc13eb7868801841d37c
SotaNorsu <pelaaja538@gmail.com>
2024-08-27 17:02:51 +0300

    push countdown

---
+++ res/js/countdown.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Aug 31, 2024 21:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  
+  document.getElementById("countdown-div").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "MVA 0.2.5.1 has been released to the public!";
+  }
+}, 1000);
