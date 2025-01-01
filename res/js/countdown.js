1d2fcbb5e107087130ec0c68e4a5c62e410d4fe9
valvex <130839843+valvexItsTaken@users.noreply.github.com>
2025-01-01 11:14:04 +0200

    new
    ficed

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("January 13, 2025 21:00:00").getTime();
+var countDownDate = new Date("January 25, 2025 12:15:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
